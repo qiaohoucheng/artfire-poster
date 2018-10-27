dc3b1a26ac3105d692cebede520111b17f4e4a0d
qiaohoucheng <qiaohoucheng@sina.com>
2018-10-27 17:57:29 +0800

    add index

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -13,7 +13,7 @@ class WeChatController extends Controller
      *
      * @return string
      */
-    public function serve()
+    public function index()
     {
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
@@ -23,5 +23,19 @@ class WeChatController extends Controller
         });
 
         return $app->server->serve();
+    }
+    public function serve()
+    {
+        $app = app('wechat.official_account');
+
+        $app->server->push(function ($message) {
+            return "您好！欢迎使用 EasyWeChat";
+        });
+
+        $response = $app->server->serve();
+
+        $response->send();
+
+        return $response;
     }
 }
