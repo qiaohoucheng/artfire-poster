76edebeebfff9d2c18d80af8f6d13c865bc7285a
qiaohoucheng <qiaohoucheng@sina.com>
2018-10-31 14:45:39 +0800

    aes 256

--- config/app.php
+++ config/app.php
@@ -105,8 +105,7 @@ return [
 
     'key' => env('APP_KEY'),
 
-    //'cipher' => 'AES-256-CBC',
-    'cipher' => 'AES-128-CBC',
+    'cipher' => 'AES-256-CBC',
     /*
     |--------------------------------------------------------------------------
     | Logging Configuration
